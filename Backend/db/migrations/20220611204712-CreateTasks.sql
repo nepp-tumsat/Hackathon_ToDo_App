ad9d304e0e059fbeedb5a86c4ece211d0f264625
PRO-active <hayamatsu8810@gmail.com>
2022-06-11 22:26:25 +0900

    [fix] migrate downの記述を修正

---
+++ Backend/db/migrations/20220611204712-CreateTasks.sql
@@ -0,0 +1,12 @@
+
+-- +migrate Up
+create table if not exists tasks (
+  id int  NOT NULL AUTO_INCREMENT primary key ,
+  user_id  int NOT NULL,
+  task varchar(255) ,
+  exp int,
+  due datetime,
+  done tinyint
+  );
+-- +migrate Down
+drop table if exists tasks;
