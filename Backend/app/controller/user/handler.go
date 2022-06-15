13ddc3925005563d0c17b1aad5af59374d7a679b
PRO-active <hayamatsu8810@gmail.com>
2022-06-15 23:07:29 +0900

    [UPDATE] 色々

--- Backend/app/controller/user/handler.go
+++ Backend/app/controller/user/handler.go
@@ -5,35 +5,31 @@ import (
 
 	"github.com/labstack/echo"
 	userModel "github.com/nepp-tumsat/Hackathon_ToDo_App/app/model/user"
-	
 )
 
 type UserHandler struct {
 	model userModel.UserModel
 }
 
-
 func NewUserHandler(u userModel.UserModel) *UserHandler {
 	return &UserHandler{
 		model: u,
 	}
 }
-func(u *UserHandler) Signup(c echo.Context) error {
+func (u *UserHandler) Signup(c echo.Context) error {
 	user := new(userModel.User)
-    // User構造体のものを作成
-    if err := c.Bind(user); err != nil {
-        return err
-    }
-    // Content-Typeに基づいて構造体にバインド
-		user.Level = 0
-    if user.Name == "" {
-        return &echo.HTTPError{
-            Code:    http.StatusBadRequest,
-            Message: "invalid name",
-        }
-    }
+	if err := c.Bind(user); err != nil {
+		return err
+	}
+	user.Level = 0
+	if user.Name == "" {
+		return &echo.HTTPError{
+			Code:    http.StatusBadRequest,
+			Message: "invalid name",
+		}
+	}
 
-    u.model.CreateUser(user)
-		
-    return c.JSON(http.StatusCreated, user)
+	u.model.CreateUser(user)
+
+	return c.JSON(http.StatusCreated, user)
 }
