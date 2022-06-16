805b785292b1bcd20bcb24f4789342b840690c2f
PRO-active <hayamatsu8810@gmail.com>
2022-06-16 16:29:34 +0900

    [ADD] タスク達成の追加

--- Backend/app/controller/task/provider.go
+++ Backend/app/controller/task/provider.go
@@ -2,11 +2,13 @@ package task
 
 import (
 	model "github.com/nepp-tumsat/Hackathon_ToDo_App/app/model/task"
+	umodel "github.com/nepp-tumsat/Hackathon_ToDo_App/app/model/user"
 	"gorm.io/gorm"
 )
 
 func InitializeTaskHandler(db *gorm.DB) *TaskHandler {
 	taskModel := model.NewTaskModel(db)
-	userHandler := NewTaskHandler(taskModel)
-	return userHandler
+	userModel := umodel.NewUserModel(db)
+	taskHandler := NewTaskHandler(taskModel, userModel)
+	return taskHandler
 }
