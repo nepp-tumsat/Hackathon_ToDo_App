90ca05f2c798474eded198d3bc1e7328a4e7c632
内山和輝 <mayachiu5g@gmail.com>
2022-06-16 23:56:24 +0900

    プロジェクト作成、ストーリーボード消去

---
+++ iOS/ToDoApp/ToDoApp/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  ToDoApp
+//
+//  Created by 内山和輝 on 2022/06/16.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
