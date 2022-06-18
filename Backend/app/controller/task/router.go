fc2ed721d11654c2279f3a86d232842950b928fb
PRO-active <hayamatsu8810@gmail.com>
2022-06-18 21:43:52 +0900

    [UPDATE] URIの修正

--- Backend/app/controller/task/router.go
+++ Backend/app/controller/task/router.go
@@ -6,7 +6,7 @@ import (
 
 func TaskRoute(e *echo.Echo, handler *TaskHandler) {
 	e.POST("/tasks", handler.CreateTask)
-	e.GET("users/:id/tasks", handler.GetAllTask)
+	e.GET("/users/:id/tasks", handler.GetAllTask)
 	e.GET("/tasks/:id", handler.GetTask)
 	e.PATCH("/tasks/:id", handler.CompleteTask)
 	e.DELETE("/tasks/:id", handler.DeleteTask)
